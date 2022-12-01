80fe9941f00311a5543562cc98d4e1e850b6d38c
Anurag Yadav <103433183+anuuraagg@users.noreply.github.com>
2022-12-01 23:19:36 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,32 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+
+const app = express();
+var items = ["Buy Food", "Cook Food", "Eat Food"];
+let workItems = [];
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+app.get("/", function(req, res){
+    var today = new Date();
+    
+    var options = {
+        weekday: "long",
+        day: "numeric",
+        month: "long"
+    };
+    var day = today.toLocaleDateString("en-US", options);
+
+    res.render("list", {listTitle: day, newListItems: items});
+});
+
+app.post("/", function(req, res){
+    let item = req.body.newItem;
+    items.push(item);
+    res.redirect("/");
+});
+
+
+app.listen(3000, function(){
+    console.log("Server started at port 3000");
+});
